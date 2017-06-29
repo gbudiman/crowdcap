204c41eeb6ce7a37b6a7a3e5a48d51947d3b97fb
Gloria Budiman <wahyu.g@gmail.com>
2017-06-29 00:42:50 -0700

    With JSON

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -2,8 +2,8 @@ class DomainsController < ApplicationController
   def build
     classes = params[:rest].split('/')
     valtrain = params[:vt]
-    images = Picture.fetch_by_class(type: valtrain, classes: classes)
+    @images = Picture.fetch_by_class(type: valtrain, classes: classes)
 
-    render partial: 'index', locals: { images: images }
+    render 'index'
   end
 end
