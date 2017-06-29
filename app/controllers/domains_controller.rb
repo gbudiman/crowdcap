c380399c8796f0381378b459a53251992b4f98aa
Gloria Budiman <wahyu.g@gmail.com>
2017-06-29 00:30:12 -0700

    Boilerplate code for domain view

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -2,7 +2,8 @@ class DomainsController < ApplicationController
   def build
     classes = params[:rest].split('/')
     valtrain = params[:vt]
+    images = Picture.fetch_by_class(type: valtrain, classes: classes)
 
-    render json: Picture.fetch_by_class(type: valtrain, classes: classes)
+    render partial: 'index', locals: { images: images }
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ApplicationRecord
   has_many :contents, through: :picture_contents
 
   def self.fetch_by_class type:, classes:
-    type_pattern = type == 'val' ? 'COCO_val2014%' : 'COCO_train2014'
+    type_pattern = type == 'val' ? 'COCO_val2014%' : 'COCO_train2014%'
 
     return Picture
       .joins(:picture_contents)
@@ -11,6 +11,6 @@ class Picture < ApplicationRecord
       .where('pictures.name LIKE :type_pattern', type_pattern: type_pattern)
       .where('contents.title IN (:classes)', classes: classes)
       .distinct
-      .pluck('pictures.id')
+      .pluck('pictures.name')
   end
 end
